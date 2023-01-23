259cda713736478cf38e7709649b58dceb3f70db
kim-seonkyu <seonkyu93@gmail.com>
2023-01-23 21:03:29 +0900

    picocss

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -6,7 +6,7 @@ import Seo from "../../components/Seo";
 export default function Blog({ data }: PageProps<Queries.BlogPostsQuery>) {
   return (
     <Layout title="Blog">
-      <section>
+      <section className="grid">
         {data.allMdx.nodes.map((file, index) => (
           <article key={index}>
             <Link to={`/blog/${file.frontmatter?.slug}`}>
@@ -17,7 +17,6 @@ export default function Blog({ data }: PageProps<Queries.BlogPostsQuery>) {
               <h6>{file.frontmatter?.date}</h6>
               <p>{file.excerpt}</p>
             </Link>
-            <hr />
           </article>
         ))}
       </section>
